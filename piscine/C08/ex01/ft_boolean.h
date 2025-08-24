893f28015be98f6fb3543530741c6d3a00272a21
Abdulraof Alkheder <aalkhede@u81z05s06.42kl.edu.my>
2025-08-25 03:34:48 +0800

    c08-ex01-norm

--- piscine/C08/ex01/ft_boolean.h
+++ piscine/C08/ex01/ft_boolean.h
@@ -6,15 +6,17 @@
 /*   By: aalkhede <aalkhede@student.42kl.edu.m      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/22 11:43:48 by aalkhede          #+#    #+#             */
-/*   Updated: 2025/08/25 03:27:29 by aalkhede         ###   ########.fr       */
+/*   Updated: 2025/08/25 03:34:28 by aalkhede         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include <unistd.h>
 
 #define EVEN(nbr) (!(nbr % 2))
 #define TRUE 1
 #define FALSE 0
 #define EVEN_MSG "I have an even number of arguments."
 #define ODD_MSG "I have an odd number of arguments."
-typedef	int	t_bool;
+
+typedef int	t_bool;
+
 #define SUCCESS 0
-#include <unistd.h>
