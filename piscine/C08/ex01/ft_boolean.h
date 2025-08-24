2212debcaa560216a11d62546444dabed0be9ca9
Abdulraof Alkheder <aalkhede@u81z05s06.42kl.edu.my>
2025-08-25 03:30:00 +0800

    C08-ex01

--- piscine/C08/ex01/ft_boolean.h
+++ piscine/C08/ex01/ft_boolean.h
@@ -6,25 +6,15 @@
 /*   By: aalkhede <aalkhede@student.42kl.edu.m      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/22 11:43:48 by aalkhede          #+#    #+#             */
-/*   Updated: 2025/08/22 11:44:03 by aalkhede         ###   ########.fr       */
+/*   Updated: 2025/08/25 03:27:29 by aalkhede         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_boolean.h"
-void ft_putstr(char *str)
-{
-while (*str)
-write(1, str++, 1);
-}
-t_bool ft_is_even(int nbr)
-{
-return ((EVEN(nbr)) ? TRUE : FALSE);
-}
-int main(int argc, char **argv)
-{
-(void)argv;
-if (ft_is_even(argc - 1) == TRUE)
-ft_putstr(EVEN_MSG);
-else
-ft_putstr(ODD_MSG);
-return (SUCCESS);
+#define EVEN(nbr) (!(nbr % 2))
+#define TRUE 1
+#define FALSE 0
+#define EVEN_MSG "I have an even number of arguments."
+#define ODD_MSG "I have an odd number of arguments."
+typedef	int	t_bool;
+#define SUCCESS 0
+#include <unistd.h>
